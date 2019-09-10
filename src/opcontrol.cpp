6c9b3b7b3663fc1fe78d255a06adca0325906f3a
Zachary Webb <ZWebb@Calvaryftl.Local>
2019-09-10 17:05:25 -0400

    added lift control

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -4,10 +4,12 @@
 using namespace pros;
 
 void opcontrol(){
-while(true){
-  chassisLF.move(leftY);
-  chassisLB.move(leftY);
-  chassisRF.move(rightY);
-  chassisRB.move(rightY);
-}
+  while(true){
+    chassisLF.move(leftY);
+    chassisLB.move(leftY);
+    chassisRF.move(rightY);
+    chassisRB.move(rightY);
+
+    lift.move((controlL1 - controlL2)*100);
+  }
 }
