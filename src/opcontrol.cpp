36cc1724abde6928ed47fee6e67724e213535acc
Zachary Webb <ZWebb@Calvaryftl.Local>
2019-09-10 17:10:05 -0400

    added intake control

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -4,6 +4,7 @@
 using namespace pros;
 
 void opcontrol(){
+  bool intaking = false;
   while(true){
     chassisLF.move(leftY);
     chassisLB.move(leftY);
@@ -11,5 +12,9 @@ void opcontrol(){
     chassisRB.move(rightY);
 
     lift.move((controlL1 - controlL2)*100);
+
+    if(controlR1) intaking = !intaking;
+    intakeL.move(200*intaking);
+    intakeR.move(200*intaking);
   }
 }
