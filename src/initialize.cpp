7fa63a812ed64f77086e777cd5aa20d2d5e8092a
Zachary Webb <ZWebb@Calvaryftl.Local>
2019-09-10 17:16:06 -0400

    intake now holds position

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -5,7 +5,8 @@ using namespace pros;
 
 void initialize() {
 	lift.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-
+	intakeL.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+	intakeR.set_brake_mode(E_MOTOR_BRAKE_HOLD);
 }
 
 void disabled() {}
